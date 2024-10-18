f6be05110f9edfe96c16ddc9b7aae4950e174b25
nikkasouza <yogikngyt@gmail.com>
2024-10-18 11:40:49 -0400

    add timeouts

--- speech_naming.py
+++ speech_naming.py
@@ -4,11 +4,12 @@ import speech_recognition as sr
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Say something!")
-    audio = r.listen(source)
+    audio = r.listen(source, timeout=3, phrase_time_limit=5)
+    
 
 # recognize speech using Sphinx
 try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    print("TEXT: " + r.recognize_sphinx(audio))
 except sr.UnknownValueError:
     print("Sphinx could not understand audio")
 except sr.RequestError as e:
