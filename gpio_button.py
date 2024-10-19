feff0d0c34372efab537cd181cd120c22b92ca6b
n-tdi <yogikngyt@gmail.com>
2024-10-19 18:33:17 -0400

    Added wireless bluetooth

--- gpio_button.py
+++ gpio_button.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import cv2
 
 # Pin definitions
 button_pin = 17  # Change to the GPIO pin you're using
@@ -8,8 +9,13 @@ button_pin = 17  # Change to the GPIO pin you're using
 GPIO.setmode(GPIO.BCM)  # Use BCM numbering
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Set up pull-up resistor
 
+cam = cv2.VideoCapture(0)
+
 def buttonPressed():
     print("Pressed!")
+    ret, image = cam.read()
+    cv2.imwrite('/home/nikkasouza/testimage.jpg', image)
+    
 
 
 try:
