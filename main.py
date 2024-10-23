b98ef6e30fccc727aea82a6eeb6d3e9e23031ded
nikkasouza <yogikngyt@gmail.com>
2024-10-23 12:40:45 -0400

    handle main

--- main.py
+++ main.py
@@ -5,14 +5,30 @@
 import os
 import sys
 import time
-import bleClient2
+import bleClient2, speech_naming, take_photo
+
+addingFace = False
 
 def button1Pressed(button: bleClient2.Button):
-    print("Button 1 pressed " + str(button.id))
+    global addingFace
+    if addingFace:
+        return None
+    addingFace = True
+    print("Adding face")
+    print("Say the name of the person")
+    name = speech_naming.transcribe(3, 3)
+    if name == None:
+        print("No name detected")
+        return None
+    print("Name detected: " + name)
+    take_photo.takePhoto(name)
+    print("Face added")
+    addingFace = False
+
     return None
 
 def button2Pressed(button: bleClient2.Button):
-    print("Button 2 pressed " + str(button.id))
+    # TODO: Implement recalling names here @Charlie
     return None
 
 bleClient2.buttons.addButton(bleClient2.Button(1, button1Pressed))
