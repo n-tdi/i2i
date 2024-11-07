afd3f57f47a2451fb2e2591812448d16a09e60a9
nikkasouza <yogikngyt@gmail.com>
2024-11-07 09:41:48 -0500

    ww

--- test_take_photo.py
+++ test_take_photo.py
@@ -5,12 +5,12 @@ video_capture = None
 
 x = 0
 while True:
+    print(x)
     video_capture = cv2.VideoCapture(x)
     if video_capture.read()[0]:
         break
     else:
         x+=1
-print(x)
 
 cv2.imwrite(f'test.jpg',video_capture.read()[1])
 print("Photo taken")
