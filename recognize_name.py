548bee0f085b4efdf408e3291914135cca7ef783
0ct0lingWithZerosForOs <0ct0lingWithZerosForOs@gmail.com>
2025-01-12 16:56:27 -0500

    qqq

--- recognize_name.py
+++ recognize_name.py
@@ -1,10 +1,12 @@
-import pyaudio
+import sounddevice
 import json
 import time
 from vosk import Model, KaldiRecognizer
+import vosk
 
 # Load the Vosk model only once to save resources
 MODEL_PATH = "vosk-model-small-en-us-0.15"
+MODEL_PATH = "C:\\Users\\marsr\\Documents\\python_class\\PythonCode\\Shrimp Lock\\Github\i2i\\vosk-model-small-en-us-0.15"
 model = Model(MODEL_PATH)
 
 def listen_for_3_seconds():
@@ -12,9 +14,8 @@ def listen_for_3_seconds():
 
     recognizer = KaldiRecognizer(model, 16000)
 
-    audio = pyaudio.PyAudio()
-    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=4096)
-    stream.start_stream()
+    audio = sounddevice.RawInputStream()
+    audio.start()
 
     print("Listening for 3 seconds...")
 
@@ -27,7 +28,7 @@ def listen_for_3_seconds():
             if time.time() - start_time > 3:
                 break
 
-            data = stream.read(4096, exception_on_overflow=False)
+            data = audio.read(audio.read_available)
             if recognizer.AcceptWaveform(data):
                 result = recognizer.Result()
                 recognized_text = json.loads(result).get("text", "")
@@ -39,9 +40,23 @@ def listen_for_3_seconds():
     except KeyboardInterrupt:
         print("\nInterrupted by user.")
     
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
+    audio.close()
 
     print("Finished listening. " + recognized_text)
     return recognized_text
+
+#print(listen_for_3_seconds())
+
+#model = vosk.Model(MODEL_PATH) 
+
+def callback(indata, frames, time, status):
+    if status:
+        #print(status, file=sys.stderr)
+        pass
+    recognizer = vosk.KaldiRecognizer(model, 16000)
+    if recognizer.AcceptWaveform(indata):
+        print(recognizer.Result())
+
+with sounddevice.RawInputStream(samplerate=16000, channels=1, dtype='int16', callback=callback):
+    while True:
+        sounddevice.sleep(100)
